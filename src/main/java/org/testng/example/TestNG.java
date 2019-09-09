ab998451da8d45f493181089d88d5580068b8ccc
javarajeshvp <javarajeshvp@gmail.com>
2019-09-08 22:37:21 -0700

    Add files via upload

---
+++ src/main/java/org/testng/example/TestNG.java
@@ -0,0 +1,28 @@
+
+package org.testng.example;
+
+import org.testng.annotations.Test;
+
+@Test
+public class TestNG {
+
+	@Test(priority = 2, description = "Learning Test NG ")
+	public void test2() {
+		System.out.print("Test case : 2 ");
+	}
+
+	@Test(priority = 3)
+	public void helloTest() {
+		System.out.print("Test case : 3");
+	}
+
+	@Test(priority = 4)
+	public void firstTest() {
+		System.out.print("Test case : 1");
+	}
+
+	@Test(priority = 1, description = "Learning Test NG 2")
+	public void myTest() {
+		System.out.print("Test case : 4 ");
+	}
+}
